c887b74493336c756ea73d56c7b9852a6eb0cde5
Bruno Lira <bruno.padovan@gmail.com>
2021-05-27 19:04:20 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Bookings and Reservations
+
+This is a repository for my booking and reservations project.
+
+- Built in Go (version 1.16)
+- Dependencies:
+  - [chi router v5.0.3](https://github.com/go-chi/chi/v5)
+  - [Alex Edwards SCS Session Management v2.4](https://github.com/alexedwards/scs/v2)
+  - [nosurf v1.1.1](https://github.com/justinas/nosurf)
+
+Following the course [Building Modern Web Applications with Go](https://www.udemy.com/course/building-modern-web-applications-with-go)
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/reziak/gobookings/pkg/config"
+	"github.com/reziak/gobookings/pkg/models"
+	"github.com/reziak/gobookings/pkg/render"
+)
+
+// Repo is the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
