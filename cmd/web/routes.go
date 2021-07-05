c3998b1275f0cffd0614d12b115250e9bdfe33c9
Bruno Lira <bruno.padovan@gmail.com>
2021-07-05 16:20:49 -0300

    Section 16 Complete

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -31,8 +31,17 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
 	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
 
+	mux.Get("/user/login", handlers.Repo.ShowLogin)
+	mux.Get("/user/logout", handlers.Repo.Logout)
+	mux.Post("/user/login", handlers.Repo.PostShowLogin)
+
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	mux.Route("/admin", func(mux chi.Router) {
+		mux.Use(Auth)
+		mux.Get("/dashboard", handlers.Repo.AdminDashboard)
+	})
 
 	return mux
 }
--- internal/render/render.go
+++ internal/render/render.go
@@ -27,6 +27,9 @@ func AddDefaulData(td *models.TemplateData, r *http.Request) *models.TemplateDat
 	td.Error = app.Session.PopString(r.Context(), "error")
 	td.Warning = app.Session.PopString(r.Context(), "warning")
 	td.CSRFToken = nosurf.Token(r)
+	if app.Session.Exists(r.Context(), "user_id") {
+		td.IsAuthenticated = 1
+	}
 	return td
 }
 
